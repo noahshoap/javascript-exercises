a45a7b6ae6c07795886076d7f09ba8db71e938a9
Noah Shoap <nshoap@vols.utk.edu>
2023-07-27 19:23:11 -0400

    js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const summation = arr.reduce( (total, item) => {
+        return total + item;
+    }, 0);
+    return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+    const rv = Array.from(arguments)
+                    .reduce( (total, el) => {
+                        return total *= el;
+                    }, 1);
+    
+    return rv;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let rv = 1;
+
+    for (let i = 1; i <= a; i++) {
+        rv *= i;
+    }
+
+    return rv;
 };
 
 // Do not edit below this line

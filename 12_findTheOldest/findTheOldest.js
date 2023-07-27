49ab40414b09d12ae8be231eb8d6fdf380b3aba6
Noah Shoap <nshoap@vols.utk.edu>
2023-07-27 19:22:57 -0400

    js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = null;
+    let age = 0;
 
+    people.forEach(person => {
+        if ("yearOfDeath" in person) {
+            let tmp = person.yearOfDeath - person.yearOfBirth;
+
+            if (tmp > age) {
+                oldest = person;
+                age = tmp;
+            }
+        } else {
+            let year = new Date().getFullYear();
+            let tmp = year - person.yearOfBirth;
+
+            if (tmp > age) {
+                oldest = person;
+                age = tmp;
+            }
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line

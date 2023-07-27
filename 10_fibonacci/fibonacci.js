a075dff2b44723240bd165399b68ecbf3a4ef546
Noah Shoap <nshoap@vols.utk.edu>
2023-07-27 19:12:33 -0400

    js exercises 2

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof(num) === "string") {
+        num = Number(num);
+    }
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    let fib = [0, 1];
+
+    while (fib.length <= num) {
+        let x = fib[fib.length - 2];
+        let y = fib[fib.length - 1];
+        let z = x + y;
+        fib.push(z);
+    }
+
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line

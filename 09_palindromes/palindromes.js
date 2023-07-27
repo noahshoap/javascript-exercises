40684cd348ca9a83cfc5a28602bfaf8e3858a46f
Noah Shoap <nshoap@vols.utk.edu>
2023-07-27 19:05:16 -0400

    js exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+function isPunctuationOrWhitespace(char) {
+    if (char === '!') {
+        return true;
+    } else if (char === '.') {
+        return true;
+    } else if (char === ',') {
+        return true;
+    } else if (char === ' ') {
+        return true;
+    }
+    return false;
+}
 
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let strArray = str.split("");
+    let noPunc = strArray.filter(char => !isPunctuationOrWhitespace(char));
+
+    let reverseStr = noPunc.reverse().join("");
+    noPunc = noPunc.reverse();
+    return reverseStr === noPunc.join("");
 };
 
 // Do not edit below this line
